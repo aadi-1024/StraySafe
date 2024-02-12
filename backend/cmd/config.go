ae8b027340b8499d4087c5824f3da54aabfc3dba
Aaditya <athakur4_be22@thapar.edu>
2024-02-12 14:16:16 +0530

    basic db connection

--- backend/cmd/config.go
+++ backend/cmd/config.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/aadi-1024/straysafe/internals/mailer"
+import (
+	"github.com/aadi-1024/straysafe/internals/db"
+	"github.com/aadi-1024/straysafe/internals/mailer"
+)
 
 type Config struct {
 	Mail *mailer.Mailer
+	Db *db.Database
 }
--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -4,16 +4,23 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/aadi-1024/straysafe/internals/db"
 	"github.com/aadi-1024/straysafe/internals/mailer"
 )
 
 var App *Config
 
 func main() {
+	var err error
 	App = &Config{}
 
 	App.Mail = mailer.NewMailer("username", "password", "localhost")
 	go App.Mail.StartService()
+	App.Db, err = db.NewDb("mongodb://localhost:27017")
+
+	if err != nil {
+		log.Println(err)
+	}
 
 	srv := http.Server{
 		Addr:    "0.0.0.0:8080",
